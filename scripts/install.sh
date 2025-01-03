902fe78f4b7fca8b1eec7614edb2624a753f562b
Thanapon Johdee <sern.dev@gmail.com>
2025-01-04 04:17:16 +0700

    feat: handing installation file path with space

--- scripts/install.sh
+++ scripts/install.sh
@@ -43,7 +43,7 @@ fi
 if ! [ -d "$script_dir/../installer" ]; then
     step "start extract installer"
     chmod u+x $script_dir/$INSTALLATION_FILE_PATH
-    eval "$script_dir/$INSTALLATION_FILE_PATH --target $script_dir/../installer --noexec"
+    eval "\"$script_dir/$INSTALLATION_FILE_PATH\" --target \"$script_dir/../installer\" --noexec"
 else
     debug "The installer already extracted"
 fi
