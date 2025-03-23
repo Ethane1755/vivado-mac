d7b66043a608a203a53d8af443d282b9e76078e5
Thanapon Johdee <sern.dev@gmail.com>
2025-03-23 07:41:17 +0700

    feat: start openFPGALoader and set auto server

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -3,6 +3,7 @@ script_dir=$(dirname -- "$(readlink -nf $0)";)
 function stop_container {
     docker kill vivado > /dev/null 2>&1
     echo "Stopped Docker container"
+    kill -9 $OPENFPGALOAD_PID
     exit 0
 }
 trap 'stop_container' INT
@@ -14,6 +15,8 @@ docker run --init --rm --network=host \
     --mount type=bind,source="$script_dir/../",target="/home/user" \
     --platform linux/amd64 ubuntu_vivado_env \
     sudo -H -u user bash scripts/startup.sh &
+
+./$script_dir/openFPGALoader --verbose-level -1 -b basys3 --xvc & OPENFPGALOAD_PID=$!
 
 # monitor vivado container
 sleep 10
